a8fe73e6a1685be823540ef9cd0146c3b4068587
Kaspar Schleiser <kaspar@schleiser.de>
2021-10-15 12:49:55 +0200

    tests passing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
+#![cfg_attr(not(test), no_std)]
 #![feature(option_result_unwrap_unchecked)]
+// features needed by our use of memoffset
+#![feature(const_ptr_offset_from, const_maybe_uninit_as_ptr, const_raw_ptr_deref)]
+#![feature(const_refs_to_cell)]
 
 use core::cell::UnsafeCell;
+
+extern crate memoffset;
+pub use memoffset::offset_of;
 
 #[derive(Debug)]
 pub struct Link {
@@ -12,7 +19,7 @@ pub struct List {
 }
 
 impl Link {
-    const unsafe fn new() -> Link {
+    const fn new() -> Link {
         Link {
             next: UnsafeCell::new(core::ptr::null()),
         }
@@ -104,7 +111,8 @@ impl List {
         if self.is_empty() {
             None
         } else {
-            self.remove(unsafe { self.tail() })
+            let tail = unsafe { &*self.tail_ptr() };
+            self.remove(tail)
         }
     }
 
@@ -134,7 +142,7 @@ impl List {
 
     /// Remove and return element
     /// Complexity: O(n)
-    pub fn remove(&self, element: &Link) -> Option<&Link> {
+    pub fn remove(&mut self, element: &Link) -> Option<&Link> {
         if self.is_empty() {
             None
         } else if unsafe { self.head_ptr() } == element as *const _ {
@@ -145,11 +153,29 @@ impl List {
             unsafe {
                 // storing element here so we can return it from the closure
                 let res = element as *const _;
-                self.find_previous(element).and_then(|x| {
-                    x.link(x.next().next());
+                if let Some(prev) = self.find_previous(element) {
+                    prev.link(prev.next().next());
+                    if self.tail_ptr() == res {
+                        self.last().link(prev);
+                    }
                     Some(&*res)
-                })
+                } else {
+                    None
+                }
             }
+        }
+    }
+
+    pub fn iter(&self) -> Iter {
+        let empty = self.is_empty();
+        Iter {
+            list: self,
+            pos: if empty {
+                core::ptr::null()
+            } else {
+                unsafe { self.head_ptr() }
+            },
+            stop: empty,
         }
     }
 }
@@ -221,6 +247,158 @@ impl List {
     }
 }
 
+pub struct Iter<'a> {
+    list: &'a List,
+    pos: *const Link,
+    stop: bool,
+}
+
+// pub struct IterMut<'a> {
+//     list: &'a mut List,
+//     pos: *const Link,
+//     stop: bool,
+// }
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = &'a Link;
+    fn next(&mut self) -> Option<&'a Link> {
+        if self.stop {
+            None
+        } else {
+            unsafe {
+                if self.list.tail_ptr() as *const _ == self.pos {
+                    self.stop = true;
+                }
+                let res = &*self.pos;
+                self.pos = res.next_ptr();
+                Some(res)
+            }
+        }
+    }
+}
+
+pub struct TypedList<T, const OFFSET: usize> {
+    list: List,
+    _phantom: core::marker::PhantomData<T>,
+}
+
+impl<T, const OFFSET: usize> TypedList<T, { OFFSET }> {
+    pub const fn new() -> Self {
+        Self {
+            list: List::new(),
+            _phantom: core::marker::PhantomData {},
+        }
+    }
+
+    pub fn is_empty(&mut self) -> bool {
+        self.list.is_empty()
+    }
+
+    pub fn lpush(&mut self, element: &mut T) {
+        let element = ((element as *mut T) as usize + OFFSET) as *mut Link;
+        self.list.lpush(unsafe { &mut *element })
+    }
+
+    pub fn rpush(&mut self, element: &mut T) {
+        let element = ((element as *mut T) as usize + OFFSET) as *mut Link;
+        self.list.rpush(unsafe { &mut *element })
+    }
+
+    pub fn lpop(&mut self) -> Option<&mut T> {
+        match self.list.lpop() {
+            None => None,
+            Some(link) => {
+                Some(unsafe { &mut *((link as *const Link as usize - OFFSET) as *mut T) })
+            }
+        }
+    }
+
+    pub fn rpop(&mut self) -> Option<&mut T> {
+        match self.list.rpop() {
+            None => None,
+            Some(link) => {
+                Some(unsafe { &mut *((link as *const Link as usize - OFFSET) as *mut T) })
+            }
+        }
+    }
+
+    pub fn lpoprpush(&mut self) {
+        self.list.lpoprpush()
+    }
+
+    pub fn remove(&mut self, element: &mut T) -> Option<&T> {
+        let element = ((element as *mut T) as usize + OFFSET) as *mut Link;
+        self.list
+            .remove(unsafe { &mut *element })
+            .and_then(|x| Some(unsafe { &*((x as *const Link as usize - OFFSET) as *mut T) }))
+    }
+
+    pub fn lpeek(&mut self) -> Option<&T> {
+        match self.list.lpeek() {
+            None => None,
+            Some(link) => Some(unsafe { &*((link as *const Link as usize - OFFSET) as *mut T) }),
+        }
+    }
+
+    pub fn rpeek(&mut self) -> Option<&T> {
+        match self.list.rpeek() {
+            None => None,
+            Some(link) => Some(unsafe { &*((link as *const Link as usize - OFFSET) as *mut T) }),
+        }
+    }
+
+    pub fn iter(&self) -> TypedIter<T> {
+        TypedIter::<T> {
+            iterator: self.list.iter(),
+            offset: OFFSET,
+            _phantom: core::marker::PhantomData::<T> {},
+        }
+    }
+
+    // pub fn iter_mut(&mut self) -> TypedIterMut<T> {
+    //     TypedIterMut::<T> {
+    //         iterator: self.list.iter(),
+    //         offset: OFFSET,
+    //         _phantom: core::marker::PhantomData::<T> {},
+    //     }
+    // }
+}
+
+pub struct TypedIter<'a, T> {
+    iterator: Iter<'a>,
+    offset: usize,
+    _phantom: core::marker::PhantomData<T>,
+}
+
+impl<'a, T: 'a> Iterator for TypedIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<&'a T> {
+        if let Some(link) = self.iterator.next() {
+            Some(unsafe { &*((link as *const Link as usize - self.offset) as *mut T) })
+        } else {
+            None
+        }
+    }
+}
+
+// pub struct TypedIter<'a, T, const OFFSET: usize> {
+//     iterator: Iter<'a>,
+//     _phantom: core::marker::PhantomData<T>,
+// }
+
+// impl<'a, T: 'a, const OFFSET: usize> Iterator for TypedIter<'a, T, OFFSET> {
+//     type Item = &'a T;
+
+//     fn next(&mut self) -> Option<&'a T> {
+//         if let Some(link) = self.iterator.next() {
+//             Some(unsafe { &*((link as *const Link as usize - OFFSET) as *const T) })
+//         } else {
+//             None
+//         }
+//     }
+// }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -230,7 +408,7 @@ mod tests {
         let mut list = List::new();
         assert!(list.lpop().is_none());
 
-        let mut node = unsafe { Link::new() };
+        let mut node = Link::new();
 
         list.lpush(&mut node);
 
@@ -244,11 +422,11 @@ mod tests {
         let mut list = List::new();
         assert!(list.lpop().is_none());
 
-        let mut node = unsafe { Link::new() };
+        let mut node = Link::new();
         list.lpush(&mut node);
         assert!(unsafe { node.next_ptr() } == &node as *const Link);
 
-        let mut node2 = unsafe { Link::new() };
+        let mut node2 = Link::new();
         list.lpush(&mut node2);
 
         assert!(unsafe { node2.next_ptr() } == &node as *const Link);
@@ -265,14 +443,14 @@ mod tests {
         let mut list = List::new();
         assert!(list.lpop().is_none());
 
-        let mut node = unsafe { Link::new() };
+        let mut node = Link::new();
         list.lpush(&mut node);
         assert!(unsafe { node.next_ptr() } == &node as *const Link);
 
-        let mut node2 = unsafe { Link::new() };
+        let mut node2 = Link::new();
         list.lpush(&mut node2);
 
-        let mut node3 = unsafe { Link::new() };
+        let mut node3 = Link::new();
         list.lpush(&mut node3);
 
         assert!(unsafe { node.next_ptr() } == &node3 as *const Link);
@@ -302,8 +480,8 @@ mod tests {
     fn test_lpoprpush() {
         let mut list = List::new();
 
-        let mut node = unsafe { Link::new() };
-        let mut node2 = unsafe { Link::new() };
+        let mut node = Link::new();
+        let mut node2 = Link::new();
 
         list.lpush(&mut node);
         list.lpush(&mut node2);
@@ -312,5 +490,193 @@ mod tests {
         assert!(list.lpop().unwrap() as *const _ == &node as *const _);
         assert!(list.lpop().unwrap() as *const _ == &node2 as *const _);
         assert!(list.lpop().is_none());
+    }
+
+    #[test]
+    fn test_rpush() {
+        let mut list = List::new();
+
+        let mut node = Link::new();
+        let mut node2 = Link::new();
+
+        list.rpush(&mut node);
+        list.rpush(&mut node2);
+
+        assert!(list.lpop().unwrap() as *const _ == &mut node as *const _);
+        assert!(list.lpop().unwrap() as *const _ == &mut node2 as *const _);
+        assert!(list.lpop().is_none());
+    }
+
+    #[test]
+    fn test_rpop() {
+        let mut list = List::new();
+
+        let mut node = Link::new();
+        let mut node2 = Link::new();
+        let mut node3 = Link::new();
+
+        list.rpush(&mut node);
+        list.rpush(&mut node2);
+        list.rpush(&mut node3);
+
+        assert!(unsafe { node.next_ptr() } == &node2 as *const Link);
+        assert!(unsafe { node2.next_ptr() } == &node3 as *const Link);
+        assert!(unsafe { node3.next_ptr() } == &node as *const Link);
+        assert!(unsafe { list.tail_ptr() == &node3 as *const Link });
+
+        assert!(list.rpop().unwrap() as *const _ == &mut node3 as *const _);
+        assert!(unsafe { node.next_ptr() } == &node2 as *const Link);
+        assert!(unsafe { node2.next_ptr() } == &node as *const Link);
+        assert!(unsafe { list.tail_ptr() == &node2 as *const Link });
+
+        assert!(list.rpop().unwrap() as *const _ == &mut node2 as *const _);
+        assert!(unsafe { node.next_ptr() } == &node as *const Link);
+        assert!(unsafe { list.tail_ptr() == &node as *const Link });
+
+        assert!(list.rpop().unwrap() as *const _ == &mut node as *const _);
+        assert!(list.is_empty());
+        assert!(list.rpop().is_none());
+    }
+
+    #[test]
+    fn test_remove_first() {
+        let mut list = List::new();
+
+        let mut node = Link::new();
+        let mut node2 = Link::new();
+        let mut node3 = Link::new();
+
+        list.rpush(&mut node);
+        list.rpush(&mut node2);
+        list.rpush(&mut node3);
+
+        assert!(list.remove(&node).is_some());
+
+        assert!(list.rpop().unwrap() as *const _ == &mut node3 as *const _);
+        assert!(list.rpop().unwrap() as *const _ == &mut node2 as *const _);
+        assert!(list.rpop().is_none());
+        assert!(list.lpop().is_none());
+    }
+
+    #[test]
+    fn test_remove_mid() {
+        let mut list = List::new();
+
+        let mut node = Link::new();
+        let mut node2 = Link::new();
+        let mut node3 = Link::new();
+
+        list.rpush(&mut node);
+        list.rpush(&mut node2);
+        list.rpush(&mut node3);
+
+        assert!(list.remove(&node2).is_some());
+
+        assert!(list.rpop().unwrap() as *const _ == &mut node3 as *const _);
+        assert!(list.rpop().unwrap() as *const _ == &mut node as *const _);
+        assert!(list.rpop().is_none());
+        assert!(list.lpop().is_none());
+    }
+
+    #[test]
+    fn test_remove_last() {
+        let mut list = List::new();
+
+        let mut node = Link::new();
+        let mut node2 = Link::new();
+        let mut node3 = Link::new();
+
+        list.rpush(&mut node);
+        list.rpush(&mut node2);
+        list.rpush(&mut node3);
+
+        assert!(list.remove(&node3).is_some());
+
+        assert!(list.rpop().unwrap() as *const _ == &mut node2 as *const _);
+        assert!(list.rpop().unwrap() as *const _ == &mut node as *const _);
+        assert!(list.rpop().is_none());
+        assert!(list.lpop().is_none());
+    }
+
+    #[test]
+    fn test_iterator() {
+        let mut list = List::new();
+
+        let mut node = Link::new();
+        let mut node2 = Link::new();
+        let mut node3 = Link::new();
+
+        list.rpush(&mut node);
+        list.rpush(&mut node2);
+        list.rpush(&mut node3);
+
+        let pointers = [
+            &mut node as *const Link,
+            &mut node2 as *const Link,
+            &mut node3 as *const Link,
+        ];
+
+        println!("pointers:");
+        for entry in pointers.iter() {
+            println!("{:x}", *entry as usize);
+        }
+
+        println!("list entries:");
+        let mut i = 0;
+        for entry in list.iter() {
+            println!("{:x}", entry as *const Link as usize);
+            assert_eq!(entry as *const Link, pointers[i]);
+            i += 1;
+        }
+        assert_eq!(i, 3);
+    }
+
+    #[test]
+    fn test_iterator_empty() {
+        let list = List::new();
+
+        for _ in list.iter() {
+            assert!(false);
+        }
+    }
+
+    #[test]
+    fn test_typed_iterator() {
+        struct Data {
+            data: u32,
+            list_entry: Link,
+        }
+
+        let mut list: TypedList<Data, { offset_of!(Data, list_entry) }> = TypedList::new();
+
+        let mut node = Data {
+            data: 0,
+            list_entry: Link::new(),
+        };
+
+        let mut node2 = Data {
+            data: 1,
+            list_entry: Link::new(),
+        };
+
+        let mut node3 = Data {
+            data: 2,
+            list_entry: Link::new(),
+        };
+
+        list.rpush(&mut node);
+        list.rpush(&mut node2);
+        list.rpush(&mut node3);
+
+        let expected = [0 as u32, 1, 2];
+
+        println!("list entries:");
+        let mut i = 0;
+        for entry in list.iter() {
+            println!("{}", entry.data);
+            assert_eq!(entry.data, expected[i]);
+            i += 1;
+        }
+        assert_eq!(i, 3);
     }
 }
